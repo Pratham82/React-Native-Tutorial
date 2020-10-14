617e613a1579c35ea0ccaf0e4019417633602956
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-14 18:25:51 +0530

    Added navigation

--- Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
@@ -1,42 +1,74 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
+import Home from "./HomeComponent";
 import Dishdetail from "./DishdetailComponent";
-import { View, Text } from "react-native";
+import { View, Platform, Text } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+
+const MenuNavigator = createStackNavigator(
+  {
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail },
+  },
+  {
+    initialRoute: "Menu",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "red",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: { color: "#fff" },
+    },
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "red",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: { color: "#fff" },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator({
+  Home: {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: "Home",
+      drawLabel: "Home",
+    },
+  },
+  Menu: {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: "Menu",
+      drawLabel: "Menu",
+    },
+  },
+});
+
+const AppContainer = createAppContainer(MainNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null,
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId,
-    });
-  }
-
   render() {
     return (
-      <View>
-        <Text style={{ textAlign: "center", marginVertical: 15 }}>
-          {" "}
-          ConFusion
-        </Text>
-        <Menu
-          dishes={this.state.dishes}
-          onPress={(dishId) => this.onDishSelect(dishId)}
-        />
-        <Dishdetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <AppContainer />
       </View>
     );
   }
